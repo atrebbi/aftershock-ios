d88696f5a12d52f9472edf619bc1d5a92147e95a
asv-crafts <hdc@asvcorp.ru>
2019-03-24 10:09:27 -0700

    Skeleton of FTNAddress class

---
+++ Aftershock/Models/FTNAddress.swift
@@ -0,0 +1,21 @@
+//
+//  FTNAddress.swift
+//  Aftershock
+//
+//  Created by Anatoly Vdovichev on 24/03/2019.
+//  Copyright © 2019 ASVCorp. All rights reserved.
+//
+
+import Foundation
+
+class FTNAddress {
+    var zone: Int = 1
+    var net: Int = -1
+    var node: Int = -1
+    var point: Int? = nil
+    var domain: String? = nil
+
+    func parse(address: String) {
+        
+    }
+}
