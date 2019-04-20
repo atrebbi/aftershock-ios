be018db388ae52f25ae289b140d82408023da928
asv-crafts <hdc@asvcorp.ru>
2019-04-20 02:07:24 -0700

    Application settings

---
+++ Aftershock/UI/Settings/SettingsBundleHelper.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsBundleHelper.swift
+//  Aftershock
+//
+//  Created by Anatoly Vdovichev on 12/04/2019.
+//  Copyright © 2019 ASVCorp. All rights reserved.
+//
+
+import Foundation
+
+class SettingBundleHelper {
+    
+    
+}
